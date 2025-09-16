32bb656b65c950a9b82e442440ca86d4b1a1be56
23f3002833 <23f3002833@ds.study.iitm.ac.in>
2025-09-16 19:36:00 +0000

    Modified the User tabl

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy() # Instance of SQLAlchemy to manage the database
 
 class User(db.Model): # Parent 1
     id = db.Column(db.Integer,primary_key = True)
-    roll_no =db.Column(db.String(),nullable = False,unique = True)
+    roll_no =db.Column(db.String(),nullable = False,unique = True, default = "Pending")
     name = db.Column(db.String(),nullable = False)
     education_level = db.Column(db.String(),nullable=False)
-    cgpa = db.Column(db.Double, nullable = False)
+    cgpa = db.Column(db.Double, nullable = False, default=0.0)
