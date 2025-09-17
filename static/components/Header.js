ad85a2cd195a9d007cb44081048cf79060e5e078
23f3002833 <23f3002833@ds.study.iitm.ac.in>
2025-09-17 11:27:29 +0000

    Done setting-up the header of the application

---
+++ static/components/Header.js
@@ -0,0 +1,13 @@
+export default {    
+    template: `
+        <div class="row ">
+
+            <div class="col-10 fs-4">                
+                <i>Data Storage and Summarizer</i> 
+            </div>     
+            <hr>             
+            
+        </div>
+    `,    
+    
+}
